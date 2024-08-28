2aabc401ff35cceaec1648194646d8f9264ee296
Saurav <sauravgamer441@gmail.com>
2024-08-28 23:03:24 +0545

    add keystroke enter to remove pop up

--- extractor.py
+++ extractor.py
@@ -4,13 +4,15 @@ import time
 
 file_path = 'codex.txt'
 
-def code_paster(file_path, pause_time=1):
+def code_paster(file_path, pause_time=10):
     with open(file_path, 'r') as file:
         for line in file:
             line = line.strip()
             pyperclip.copy(line)
             time.sleep(pause_time)
             pyautogui.hotkey('ctrl', 'v')
+            pyautogui.press('enter')
+            time.sleep(pause_time)
             pyautogui.press('enter')
             time.sleep(pause_time)
             pyautogui.hotkey('ctrl', 'a')
