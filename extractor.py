2c9ca5cbbda7986cf36c757fec2834291042e8af
Saurav <sauravgamer441@gmail.com>
2024-08-28 23:01:39 +0545

    add cpy paste

--- extractor.py
+++ extractor.py
@@ -2,5 +2,20 @@ import pyperclip
 import pyautogui
 import time
 
+file_path = 'codex.txt'
 
-pyautogui.alert("pyauto test")
+def code_paster(file_path, pause_time=1):
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip()
+            pyperclip.copy(line)
+            time.sleep(pause_time)
+            pyautogui.hotkey('ctrl', 'v')
+            pyautogui.press('enter')
+            time.sleep(pause_time)
+            pyautogui.hotkey('ctrl', 'a')
+            pyautogui.press('backspace')
+            time.sleep(pause_time)
+
+
+code_paster('codes.txt')
